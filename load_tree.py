f090543430eac0fd5704d521257432ee0d06f2d3
Ivan Alsina <ivanalsina@protonmail.com>
2022-03-31 08:33:58 -0500

    Node tree can now be created from json

--- load_tree.py
+++ load_tree.py
@@ -23,8 +23,46 @@ def load_paths():
 		return [el.replace('\n', '') for el in f.readlines()]
 
 
+def load_tree():
+
+	return get_tree(load_json())
+
+
+def get_tree(inp):
+
+	assert isinstance(inp, list) or isinstance(inp, dict) or isinstance(inp, str)
+
+	if isinstance(inp, list):
+
+		for el in inp:
+
+			n = get_tree(el)
+
+	elif isinstance(inp, dict):
+
+		assert len(inp) == 1
+		key, val = list(inp.items())[0]
+		assert isinstance(val, list)
+
+		n = Node(key, [get_tree(el) for el in val])
+
+
+	elif isinstance(inp, str):
+
+		n = Node(inp)
+
+	return n
+
+
+
+	
+
+
 if __name__ == "__main__":
 
 	tree_json = load_json()
 	tree_paths = load_paths()
 	tree = load_obj()
+	j = tree_json
+	a = tree_json['/'][0]
+	t = load_tree()
